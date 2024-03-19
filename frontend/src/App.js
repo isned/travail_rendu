65c83050db0a4d23879cb3a737f916b82320183c
isned belhadj <esned.blhj@gmail.com>
2024-03-19 13:37:42 +0000

    Initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,25 @@
-import React from "react";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-import SignUp from "./components/signup_component";
-import Login from "./components/login_component";
-import AdminHome from "./components/admin_component";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const isLoggedIn = window.localStorage.getItem("loggedIn");
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={isLoggedIn == "true" ? <SignUp /> : <Login />}
-          />
-          <Route path="/sign-in" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/admin" element={<AdminHome />} />
-        
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
 export default App;
-
-
